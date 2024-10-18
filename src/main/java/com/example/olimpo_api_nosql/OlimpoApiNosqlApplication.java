af49a4cfa316bc2b58ae557a964d13cdd347eb98
RaphaCosil <Raphael.Cosil@gmail.com>
2024-10-18 09:39:31 -0300

    ADD - initial project

---
+++ src/main/java/com/example/olimpo_api_nosql/OlimpoApiNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.olimpo_api_nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlimpoApiNosqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlimpoApiNosqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/olimpo_api_nosql/OlimpoApiNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.olimpo_api_nosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlimpoApiNosqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
